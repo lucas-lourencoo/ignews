d24096a9a5fe152dbd21639929dd793b4016a688
Lucas Lourenço Silva <lucascelestiano@gmail.com>
2022-01-12 10:34:42 -0400

    main

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,3 +1,20 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
 export function SignInButton() {
-  return;
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton}>
+      <FaGithub color='#04d361' />
+      Lucas Lourenço
+      <FiX color='#73738b' className={styles.rightIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton}>
+      <FaGithub color='#eba427' />
+      Sign In With Github
+    </button>
+  );
 }
